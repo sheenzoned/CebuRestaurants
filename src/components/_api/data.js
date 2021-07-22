21b5afd87c1311b4762b42821a0e9a677417e412
Sheen Carbellido <sheencarbellido@Sheens-MacBook-Pro.local>
2021-07-22 18:49:00 +0800

    Add data

---
+++ src/components/_api/data.js
@@ -0,0 +1,267 @@
+export const restaurants = [
+  {
+    place_id: "1",
+    name: "Bok Chicken",
+    geometry: {
+      location: {
+        lat: 10.3166992,
+        lng: 123.8864366,
+      },
+      viewport: {
+        northeast: {
+          lat: 10.31805352989272,
+          lng: 123.8878056798927,
+        },
+        southwest: {
+          lat: 10.31535387010728,
+          lng: 123.8851060201073,
+        },
+      },
+    },
+    foodSpecialty: ["Chicken Wings"],
+    visits: 1,
+    type: "FASTFOOD",
+  },
+  {
+    place_id: "2",
+    name: "Oishi",
+    geometry: {
+      location: {
+        lat: 10.332365,
+        lng: 123.906484,
+      },
+      viewport: {
+        northeast: {
+          lat: 10.41705342989272,
+          lng: 123.9868056798927,
+        },
+        southwest: {
+          lat: 10.41435377010728,
+          lng: 123.9841060201073,
+        },
+      },
+    },
+    foodSpecialty: ["Ramen", "Chicken Karaage"],
+    visits: 1,
+    type: "FASTFOOD",
+  },
+  {
+    place_id: "3",
+    name: "Tulip's Cafe",
+    geometry: {
+      location: {
+        lat: 10.3256992,
+        lng: 123.8954366,
+      },
+      viewport: {
+        northeast: {
+          lat: 10.32705352989272,
+          lng: 123.8968056798927,
+        },
+        southwest: {
+          lat: 10.32435387010728,
+          lng: 123.8941060201073,
+        },
+      },
+    },
+    foodSpecialty: [
+      "Blueberry Cheesecake",
+      "Red Velvet Cupcakes",
+      "Strawberry Smoothie",
+    ],
+    visits: 1,
+    type: "CAFE",
+  },
+  {
+    place_id: "4",
+    name: "Cafe by the Sea",
+    geometry: {
+      location: {
+        lat: 10.3187261,
+        lng: 124.0315874,
+      },
+      viewport: {
+        northeast: {
+          lat: 10.32705352989272,
+          lng: 123.8968056798927,
+        },
+        southwest: {
+          lat: 10.32435387010728,
+          lng: 123.8941060201073,
+        },
+      },
+    },
+    foodSpecialty: ["Fruit Cake", "Tropical Yoghurt Bowl"],
+    visits: 1,
+    type: "CAFE",
+  },
+  {
+    place_id: "5",
+    name: "Highlands",
+    geometry: {
+      location: {
+        lat: 10.3508596,
+        lng: 123.8794101,
+      },
+      viewport: {
+        northeast: {
+          lat: 10.32705352989272,
+          lng: 123.8968056798927,
+        },
+        southwest: {
+          lat: 10.32435387010728,
+          lng: 123.8941060201073,
+        },
+      },
+    },
+    foodSpecialty: ["Vegetarian Panini"],
+    visits: 1,
+    type: "CAFE",
+  },
+  {
+    place_id: "6",
+    name: "Dessance",
+    geometry: {
+      location: {
+        lat: 10.3206497,
+        lng: 123.9345746,
+      },
+      viewport: {
+        northeast: {
+          lat: 10.32705352989272,
+          lng: 123.8968056798927,
+        },
+        southwest: {
+          lat: 10.32435387010728,
+          lng: 123.8941060201073,
+        },
+      },
+    },
+    foodSpecialty: ["Beef Tenderloin"],
+    visits: 1,
+    type: "FINE_DINING",
+  },
+  {
+    place_id: "7",
+    name: "Bon Appetit",
+    geometry: {
+      location: {
+        lat: 10.3232277,
+        lng: 123.970832,
+      },
+      viewport: {
+        northeast: {
+          lat: 10.32705352989272,
+          lng: 123.8968056798927,
+        },
+        southwest: {
+          lat: 10.32435387010728,
+          lng: 123.8941060201073,
+        },
+      },
+    },
+    foodSpecialty: ["Mini Quiche With Broccoli"],
+    visits: 1,
+    type: "FINE_DINING",
+  },
+  {
+    place_id: "8",
+    name: "L'Assaggio",
+    geometry: {
+      location: {
+        lat: 10.3726023,
+        lng: 123.8686403,
+      },
+      viewport: {
+        northeast: {
+          lat: 10.32705352989272,
+          lng: 123.8968056798927,
+        },
+        southwest: {
+          lat: 10.32435387010728,
+          lng: 123.8941060201073,
+        },
+      },
+    },
+    foodSpecialty: ["Spaghetti alla Puttanesca", "Lasagna bolognese"],
+    visits: 1,
+    type: "FINE_DINING",
+  },
+  {
+    place_id: "9",
+    name: "Grillers",
+    geometry: {
+      location: {
+        lat: 10.2699462,
+        lng: 123.8770934,
+      },
+      viewport: {
+        northeast: {
+          lat: 10.32705352989272,
+          lng: 123.8968056798927,
+        },
+        southwest: {
+          lat: 10.32435387010728,
+          lng: 123.8941060201073,
+        },
+      },
+    },
+    foodSpecialty: ["Grilled Chicken and Cheese"],
+    visits: 1,
+    type: "CASUAL_DINING",
+  },
+  {
+    place_id: "10",
+    name: "Unli Bites",
+    geometry: {
+      location: {
+        lat: 10.304149,
+        lng: 123.911941,
+      },
+      viewport: {
+        northeast: {
+          lat: 10.32705352989272,
+          lng: 123.8968056798927,
+        },
+        southwest: {
+          lat: 10.32435387010728,
+          lng: 123.8941060201073,
+        },
+      },
+    },
+    foodSpecialty: ["Chicken Mozzarella", "Chicken Cordon Bleu"],
+    visits: 1,
+    type: "BUFFET",
+  },
+  {
+    place_id: "11",
+    name: "Mukbang",
+    geometry: {
+      location: {
+        lat: 10.349879,
+        lng: 123.914344,
+      },
+      viewport: {
+        northeast: {
+          lat: 10.32705352989272,
+          lng: 123.8968056798927,
+        },
+        southwest: {
+          lat: 10.32435387010728,
+          lng: 123.8941060201073,
+        },
+      },
+    },
+    foodSpecialty: ["Bulgogi", "Dakgalbi"],
+    visits: 1,
+    type: "BUFFET",
+  },
+];
+
+export const restaurantType = [
+  { id: "FINE_DINING", name: "Fine Dining", checked: true },
+  { id: "CASUAL_DINING", name: "Casual Dining", checked: true },
+  { id: "BUFFET", name: "Buffet", checked: true },
+  { id: "CAFE", name: "Cafe", checked: true },
+  { id: "FAST_FOOD", name: "Fast Food", checked: true },
+];
